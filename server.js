fbb5ee19cb6515ba05bfa2de4581cf372c36c5c2
Adam Brault <adam@andyet.net>
2013-05-20 01:39:39 -0700

    adding everything, still all work in progress

---
+++ server.js
@@ -0,0 +1,48 @@
+var express = require('express'),
+    connect = require('connect'),
+    logger = require('winston'),
+    semiStatic = require('semi-static'),
+    config = require('getconfig');
+
+
+var views = require('./serverapp/views');
+
+var app = express();
+
+app.configure(function () {
+    app.use(express.compress());
+    app.use(express['static'](__dirname + '/public'));
+    // app.use(express.favicon(__dirname + '/public/images/favicon.ico'));
+    app.use(express.bodyParser());
+});
+
+// use jade
+app.set('view engine', 'jade');
+
+// 301 redirects for old tumblr blog posts
+app.get('/post/:tid/:tslug', function(req, res) {
+    var slug = req.params.tslug;
+    res.redirect(301, slug)
+    logger.info('Request:  ' + config.baseUrl + req.url + '\n>>>>> Redirect: ' + config.baseUrl + '/' + slug)
+});
+
+// blog post index
+app.get('/blog', views.blogIndex);
+
+// blog posts
+app.get('/blog/:pslug', views.blogPost);
+
+// TODO
+// quotes, talks, links, tools, apps, music, micro
+
+// app views
+app.get('/', views.index);
+
+// semi-static views
+app.get('/*', semiStatic());
+
+// 404
+app.get('*', views.notFound);
+
+app.listen(config.http.port);
+logger.info('node server running on port: ' + config.http.port);
---
+++ serverapp/views.js
@@ -0,0 +1,58 @@
+var path = require('path'),
+  logger = require('winston'),
+  walk = require('walk'),
+  marked = require('marked')
+  // slug = require('slugger'), TODO — why can't it find this?
+  fs = require('fs')
+
+// homepage
+exports.index = function (req, res) {
+  res.render('index', {pageTitle: 'home', bodyId: 'home'});
+}; 
+
+exports.blogIndex = function (req, res) {
+
+  var walker = walk.walk('blog'),
+      files = new Array(),
+      urls = new Array(),
+      fileName,
+      slug;
+
+  walker.on("file", function(root,file,next){
+    // get the file, but remove file extension
+    var file = file['name'].substring(0, file['name'].lastIndexOf('.'));
+    // push without /blog prefix
+    fileName = file.substring(file.indexOf('/'));
+    // slug = slug.slugger(file)
+    files.push(fileName);
+    next();
+  });
+
+  walker.on("end", function() {
+    res.render('blogIndex', { 
+      pageTitle: 'All posts', 
+      bodyId: 'archive',
+      postList: files,
+      urlPath: 'blog/'
+    });
+  });
+
+};
+
+exports.blogPost = function (req, res) {
+  var slug = req.params.pslug;
+  var post = fs.readFileSync('blog/' + slug + ".md");
+  var markdown = marked(fs.readFileSync('blog/' + slug + ".md"));
+  res.render('post', {
+    pageTitle: 'blog', 
+    bodyId: 'post',
+    slug: slug,
+    content: markdown
+  });
+  logger.info('Blog post requested: ' + slug);
+};
+
+// 404
+exports.notFound = function (req, res) {
+  res.render('404', {status: 404, bodyId: 'fourohfour'});
+};
