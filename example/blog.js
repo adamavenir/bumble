d7ca989bfebc55163e320b0d91f33769d6f1d36e
Adam Brault <adam@andyet.net>
2014-01-15 20:14:37 -0500

    fixed paginator bug

--- example/blog.js
+++ example/blog.js
@@ -7,9 +7,7 @@ var app = express();
 app.configure(function () {
     app.use(express.compress());
     app.use(express['static'](__dirname + '/public'));
-    app.use(express.favicon(__dirname + '/public/images/favicon.ico'));
     app.use(express.bodyParser());
-    app.use(logger.middleware());
 });
 
 var bumble = new Bumble(app, config);
