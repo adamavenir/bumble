c1f099c0b9e3d71701bc97186c770d827c2be19e
Michael Garvin <gar@andyet.net>
2014-03-21 22:16:51 -0700

    Start tests

---
+++ Makefile
@@ -0,0 +1,10 @@
+test-cof:
+		@node node_modules/lab/bin/lab -r threshold -t 100
+test:
+		@node node_modules/lab/bin/lab
+test-cov-html:
+		@node node_modules/lab/bin/lab -r html -o coverage.html
+complexity:
+		@node node_modules/complexity-report/src/index.js -o complexity.md -f markdown lib/bumble.js
+
+.PHONY: test test-no-cov test-cov-html complexity
---
+++ test/defaults.js
@@ -0,0 +1,28 @@
+/* Test that the handlers return expected data */
+var server;
+var Lab = require('lab');
+var Hapi = require('hapi');
+var blogConfig = require('./testConfig.json');
+
+Lab.experiment('default happy path tests', function () {
+    Lab.before(function (done) {
+        server = new Hapi.Server('localhost', 3001);
+        server.pack.require({'..': blogConfig}, function _packRequired(err) {
+            if (err) {
+                process.stderr.write('Unable to setUp tests', err, '\n');
+                process.exit(1);
+            }
+            done();
+        });
+    });
+    Lab.test('tumblr redirect', function (done) {
+        server.inject({
+            method: 'get',
+            url: '/post/xj9000/another-post-goes-here'
+        }, function _getPost(res) {
+            Lab.expect(res.statusCode, 'response code').to.equal(301);
+            Lab.expect(res.headers.location, 'redirect location').to.equal('http://localhost:3001/2011/06/04/another-post-goes-here');
+            done();
+        });
+    });
+});
